bfee4f991469a10f872c23acc527057795415f32
Valor Zhong <valorzhong@gmail.com>
2013-09-16 01:06:17 +0800

    add Sphere

--- lib/src/primitives.dart
+++ lib/src/primitives.dart
@@ -99,6 +99,90 @@ class Cube extends Mesh {
   }
 }
 
+// from J3D
+class Sphere extends Mesh {
+  Sphere(num radius, num segmentsWidth, num segmentsHeight) {
+    
+    var segmentsX = math.max(3, segmentsWidth.floor());
+    var segmentsY = math.max(3, segmentsHeight.floor());
+    
+    var phiStart = 0;
+    var phiLength = math.PI * 2;
+    
+    var thetaStart = 0;
+    var thetaLength = math.PI;
+    
+    var x, y;
+    
+    var vertices = [];
+    var uvs = [];
+    for(y = 0; y <= segmentsY; y++) {
+      for(x = 0; x <= segmentsX; x++) {
+        var u = x / segmentsX;
+        var v = y / segmentsY;
+        var xp = -radius * math.cos(phiStart + u * phiLength) * math.sin(thetaStart + v * thetaLength);
+        var yp = radius * math.cos(thetaStart + v * thetaLength);
+        var zp = radius * math.sin(phiStart + u * phiLength) * math.sin(thetaStart + v * thetaLength);
+        
+        vertices.add(new Vector3(xp, yp, zp));
+        uvs.add(new Vector2(u, 1 - v));
+      }
+    }
+
+    _geometry = new Geometry();
+    _geometry.vertices = [];
+    _geometry.textureCoords = [];
+    _geometry.normals = [];
+    _faces = [];
+    for(y = 0; y < segmentsY; y++) {
+      for(x = 0; x < segmentsX; x++) {
+        var o = segmentsX + 1;
+        var vt1 = vertices[y * o + x + 0];
+        var vt2 = vertices[y * o + x + 1];
+        var vt3 = vertices[(y + 1) * o + x + 1];
+        var vt4 = vertices[(y + 1) * o + x + 0];
+
+        Vector2 uv1 = uvs[ y * o + x + 0 ];
+        var uv2 = uvs[ y * o + x + 1 ];
+        var uv3 = uvs[ (y + 1) * o + x + 1 ];
+        var uv4 = uvs[ (y + 1) * o + x + 0 ];
+        
+        var n1 = vt1.clone().normalize();
+        var n2 = vt2.clone().normalize();
+        var n3 = vt3.clone().normalize();
+        var n4 = vt4.clone().normalize();
+
+        var p = (_geometry.vertices.length / 3).floor();
+
+        _geometry.vertices.addAll([vt1.x, vt1.y, vt1.z, vt2.x, vt2.y, vt2.z, vt3.x, vt3.y, vt3.z, vt4.x, vt4.y, vt4.z]);
+        _geometry.textureCoords.addAll([uv1[0], uv1[1], uv2[0], uv2[1], uv3[0], uv3[1], uv4[0], uv4[1]]);
+        _geometry.normals.addAll([n1.x, n1.y, n1.z, n2.x, n2.y, n2.z, n3.x, n3.y, n3.z, n4.x, n4.y, n4.z]);
+        _faces.addAll([p + 0, p + 1, p + 2, p + 0, p + 2, p + 3]);
+      }
+    }
+    
+    _material = new Material();
+    _material.shader = Shader.simpleColorShader;
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

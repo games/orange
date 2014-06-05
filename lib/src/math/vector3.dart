8128ae0584b261ef374f910bc1b925b0ae920ad3
Valor Zhong <valorzhong@gmail.com>
2014-06-05 11:04:44 +0800

    setZero method

--- lib/src/math/vector3.dart
+++ lib/src/math/vector3.dart
@@ -3,24 +3,26 @@ part of orange;
 
 class Vector3 {
   final Float32List storage = new Float32List(3);
-  
+
   Vector3.zero();
-  
+
   Vector3(double x_, double y_, double z_) {
     setValues(x_, y_, z_);
   }
-  
+
   Vector3.fromList(List<num> list) {
     setValues(list[0].toDouble(), list[1].toDouble(), list[2].toDouble());
   }
-  
+
   setValues(double x_, double y_, double z_) {
     storage[0] = x_;
     storage[1] = y_;
     storage[2] = z_;
     return this;
   }
-  
+
+  setZero() => setValues(0.0, 0.0, 0.0);
+
   Vector3 clone() {
     var vec = new Vector3.zero();
     vec.storage[0] = storage[0];
@@ -28,32 +30,32 @@ class Vector3 {
     vec.storage[2] = storage[2];
     return vec;
   }
-  
+
   double get x => storage[0];
   double get y => storage[1];
   double get z => storage[2];
-  
+
   double get length {
     var x = storage[0],
         y = storage[1],
         z = storage[2];
     return math.sqrt(x * x + y * y + z * z);
   }
-  
+
   double get squaredLength {
     var x = storage[0],
         y = storage[1],
         z = storage[2];
     return x * x + y * y + z * z;
   }
-  
+
   Vector3 add(Vector3 arg) {
     storage[0] = storage[0] + arg.storage[0];
     storage[1] = storage[1] + arg.storage[1];
     storage[2] = storage[2] + arg.storage[2];
     return this;
   }
-  
+
   Vector3 normalize() {
     double l = length;
     if (l == 0.0) {
@@ -65,7 +67,7 @@ class Vector3 {
     storage[2] *= l;
     return this;
   }
-  
+
   /// Inner product.
   double dot(Vector3 other) {
     double sum;
@@ -85,33 +87,31 @@ class Vector3 {
     double oz = other.storage[2];
     return new Vector3(_y * oz - _z * oy, _z * ox - _x * oz, _x * oy - _y * ox);
   }
-  
+
   /// Negate
-  Vector3 operator-() => new Vector3(- storage[0], - storage[1], - storage[2]);
+  Vector3 operator -() => new Vector3(-storage[0], -storage[1], -storage[2]);
 
   /// Subtract two vectors.
-  Vector3 operator-(Vector3 other) => new Vector3(storage[0] - other.storage[0],
-                                         storage[1] - other.storage[1],
-                                         storage[2] - other.storage[2]);
+  Vector3 operator -(Vector3 other) => new Vector3(storage[0] - other.storage[0], storage[1] - other.storage[1], storage[2] - other.storage[2]);
   /// Scale.
-  Vector3 operator/(double scale) {
+  Vector3 operator /(double scale) {
     var o = 1.0 / scale;
     return new Vector3(storage[0] * o, storage[1] * o, storage[2] * o);
   }
 
   /// Scale.
-  Vector3 operator*(double scale) {
+  Vector3 operator *(double scale) {
     var o = scale;
     return new Vector3(storage[0] * o, storage[1] * o, storage[2] * o);
   }
-  
-  double operator[](int i) => storage[i];
-  void operator[]=(int i, double v) {
+
+  double operator [](int i) => storage[i];
+  void operator []=(int i, double v) {
     storage[i] = v;
   }
-  
-  Vector3 operator+(Vector3 other) => clone().add(other);
-  
+
+  Vector3 operator +(Vector3 other) => clone().add(other);
+
   toString() {
     return "vec3($x, $y, $z)";
   }
@@ -128,7 +128,7 @@ class Vector3 {
  * @param {Number} t interpolation amount between the two inputs
  * @returns {vec3} out
  */
-Vector3 lerp (Vector3 a, Vector3 b, double t) {
+Vector3 lerp(Vector3 a, Vector3 b, double t) {
   var out = new Vector3.zero();
   var ax = a[0],
       ay = a[1],
@@ -138,7 +138,6 @@ Vector3 lerp (Vector3 a, Vector3 b, double t) {
   out[2] = az + t * (b[2] - az);
   return out;
 }
-
 
 
 

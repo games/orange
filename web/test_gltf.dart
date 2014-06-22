2c5f8b235f567dccbec37947123ba8a0b4329c98
Valor Zhong <valorzhong@gmail.com>
2014-06-23 02:35:06 +0800

    controls

--- web/test_gltf.dart
+++ web/test_gltf.dart
@@ -1,5 +1,6 @@
 import 'package:orange/orange.dart';
 import 'package:vector_math/vector_math.dart';
+import 'dart:html';
 
 
 
@@ -7,24 +8,30 @@ class TestGLTFScene extends Scene {
 
   TestGLTFScene(Camera camera) : super(camera);
 
-
+  List _meshes = [];  
+  
   @override
   void enter() {
 
-//    camera.position.setValues(-20.0, 2600.0, 1000.0);
+    //    camera.position.setValues(-20.0, 2600.0, 1000.0);
     camera.position.setValues(100.0, 100.0, 300.0);
     camera.lookAt(new Vector3.zero());
 
-    var url = "../models/duck/duck.json";
-//    url = "../models/SuperMurdoch/SuperMurdoch.json";
-//    url = "../models/rambler/rambler.json";
-    
-    var loader = new GltfLoader2();
-    loader.load(graphicsDevice.ctx, url).then((nodes) {
-      nodes.forEach((node) {
-        add(node);
-      });
+    var urls = ["../models/duck/duck.json", "../models/SuperMurdoch/SuperMurdoch.json", "../models/rambler/rambler.json", "../models/wine/wine.json"];
+
+    var selector = new SelectElement();
+    urls.forEach((u) {
+      var option = new OptionElement(data: u.split("/").last, value: u);
+      selector.children.add(option);
+      
     });
+    selector.onChange.listen((e) {
+      var opt = selector.options[selector.selectedIndex];
+      _loadModel(opt.value);
+    });
+    querySelector("#controllers").children.add(selector);
+
+    _loadModel(urls.first);
 
     var light0 = new PointLight(0xffffff);
     light0.intensity = 0.9;
@@ -38,7 +45,18 @@ class TestGLTFScene extends Scene {
     light1.direction = new Vector3(-1.0, -1.0, 0.0).normalize();
     add(light1);
   }
-  
+
+  void _loadModel(String url) {
+    var loader = new GltfLoader2();
+    loader.load(graphicsDevice.ctx, url).then((nodes) {
+      _meshes.forEach((m) => remove(m));
+      nodes.forEach((node) {
+        add(node);
+      });
+      _meshes = nodes;
+    });
+  }
+
   @override
   void enterFrame(num elapsed, num interval) {
   }

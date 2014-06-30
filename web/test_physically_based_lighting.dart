fa8286a8714a9de6eea97720f29dedc0c87eadb5
DDD <DDD@gmail.com>
2014-07-01 03:07:54 +0800

    physically based lighting update

--- web/test_physically_based_lighting.dart
+++ web/test_physically_based_lighting.dart
@@ -9,6 +9,10 @@ part of orange_examples;
 
 class TestPhysicallyBasedLighting extends Scene {
   TestPhysicallyBasedLighting(Camera camera) : super(camera);
+  
+  int type = 0;
+  double roughness = 0.5;
+  double specualerColor = 0.5;
 
   @override
   void enter() {
@@ -16,10 +20,6 @@ class TestPhysicallyBasedLighting extends Scene {
                     "models/obj/head.obj", 
                     "models/obj/train.obj", 
                     "models/obj/female02/female02.obj", 
-                    "models/obj/cow-nonormals.obj", 
-                    "models/obj/pumpkin_tall_10k.obj", 
-                    "models/obj/teapot.obj", 
-                    "models/obj/teddy.obj",
                     "models/obj/tree.obj"];
     
     var selector = new html.SelectElement();
@@ -31,9 +31,61 @@ class TestPhysicallyBasedLighting extends Scene {
       var opt = selector.options[selector.selectedIndex];
       _loadModel(opt.value);
     });
-    html.querySelector("#controllers").children.add(selector);
+    var controllers = html.querySelector("#controllers");
+    controllers.children.add(selector);
+    controllers.children.add(new html.BRElement());
     
-    _loadModel("models/obj/head.obj");
+    
+    var types = new html.SelectElement();
+    for(var i = 0; i < 7; i++) {
+      var option = new html.OptionElement(data: i.toString(), value: i.toString());
+      types.children.add(option);
+     }
+    types.onChange.listen((e) {
+      var opt = types.options[types.selectedIndex];
+      type = int.parse(opt.value);
+    });
+    controllers.children.add(_labelFor(types, "types"));
+    controllers.children.add(new html.BRElement());
+    controllers.children.add(types);
+    controllers.children.add(new html.BRElement());
+    
+    var roughnessSlider = new html.RangeInputElement();
+    roughnessSlider.id = "roughness";
+    roughnessSlider.min = "0";
+    roughnessSlider.max = "1";
+    roughnessSlider.step = "0.01";
+    roughnessSlider.value = roughness.toString();
+    roughnessSlider.onChange.listen((e) {
+      roughness = double.parse(roughnessSlider.value);
+    });
+    controllers.children.add(_labelFor(roughnessSlider, "Roughness(0~1)"));
+    controllers.children.add(new html.BRElement());
+    controllers.children.add(roughnessSlider);
+    controllers.children.add(new html.BRElement());
+    
+    var specualrSlider = new html.RangeInputElement();
+    specualrSlider.id = "roughness";
+    specualrSlider.min = "0";
+    specualrSlider.max = "3";
+    specualrSlider.step = "0.01";
+    specualrSlider.value = specualerColor.toString();
+    specualrSlider.onChange.listen((e) {
+      specualerColor = double.parse(specualrSlider.value);
+    });
+    controllers.children.add(_labelFor(specualrSlider, "Specualer(0~3)"));
+    controllers.children.add(new html.BRElement());
+    controllers.children.add(specualrSlider);
+    controllers.children.add(new html.BRElement());
+    
+    _loadModel(urls.first);
+  }
+  
+  html.LabelElement _labelFor(html.Element element, String title) {
+    var label = new html.LabelElement();
+    label.htmlFor = element.id;
+    label.text = title;
+    return label;
   }
 
   void _loadModel(String url) {
@@ -43,11 +95,21 @@ class TestPhysicallyBasedLighting extends Scene {
       add(m);
       
       var envTexture = new CubeTexture("textures/cube/Bridge2/bridge");
+      var texture = Texture.load(graphicsDevice.ctx, {
+        "path": "models/obj/skin1.jpg"
+      });
+      var ggx = Texture.load(graphicsDevice.ctx, {
+        "path": "textures/ggx-helper-dfv.png"
+      });
 
       m.material = new ShaderMaterial(graphicsDevice, PhysicallyVS, PhysicallyFS);
       m.material.backFaceCulling = false;
       (m.material as ShaderMaterial).afterBinding = (ShaderMaterial material, Mesh mesh, Matrix4 worldMatrix) {
-        graphicsDevice.bindTexture("env", envTexture);
+        graphicsDevice.bindTexture("GgxDFV", ggx);
+        graphicsDevice.bindTexture("diffuseSampler", texture);
+        graphicsDevice.bindInt("type", type);
+        graphicsDevice.bindFloat("uRoughess", roughness);
+        graphicsDevice.bindFloat("uSpecularColour", specualerColor);
       };
       var box = m.boundingInfo.boundingBox;
       var radius = m.boundingInfo.boundingSphere.radius;
@@ -71,6 +133,7 @@ precision mediump float;
 
 attribute vec3 position;
 attribute vec3 normal;
+attribute vec2 uv;
 uniform mat4 world;
 uniform mat4 view;
 uniform mat4 viewProjection;
@@ -79,11 +142,13 @@ uniform vec3 vEyePosition;
 
 varying vec3 vWorldPosition;
 varying vec3 vNormal;
+varying vec2 vUV;
 
 void main(void) {
   vec4 wp = world * vec4(position, 1.0);
   vWorldPosition = wp.xyz; 
   vNormal = normalize(vec3(world * vec4(normal, 0.0)));
+  vUV = uv;
  
   gl_Position = viewProjection * wp;
 }
@@ -92,48 +157,264 @@ void main(void) {
 // http://www.altdev.co/2011/08/23/shader-code-for-physically-based-lighting/
 const PhysicallyFS = """
 precision mediump float;
+precision mediump int;
 
 #define PI 3.1415926535897932384626433832795
 #define PI_OVER_TWO 1.5707963267948966
 #define PI_OVER_FOUR 0.7853981633974483
 
 uniform vec3 vEyePosition;
+uniform sampler2D GgxDFV;
+uniform sampler2D diffuseSampler;
+uniform int type;
+uniform float uRoughess;
+uniform float uSpecularColour;
 
 varying vec3 vWorldPosition;
 varying vec3 vNormal;
+varying vec2 vUV;
+
+float saturate(float d) {
+  return clamp(d, 0.0, 1.0);
+}
+
+float G1V(float dotNV, float k) {
+  return 1.0/(dotNV*(1.0-k)+k);
+}
+
+float LightingFuncGGX_REF(vec3 N, vec3 V, vec3 L, float roughness, float F0)
+{
+  float alpha = roughness*roughness;
+  vec3 H = normalize(V+L);
+
+  float dotNL = saturate(dot(N,L));
+  float dotNV = saturate(dot(N,V));
+  float dotNH = saturate(dot(N,H));
+  float dotLH = saturate(dot(L,H));
+
+  float F, D, vis;
+
+  // D
+  float alphaSqr = alpha*alpha;
+  float pi = 3.14159;
+  float denom = dotNH * dotNH *(alphaSqr-1.0) + 1.0;
+  D = alphaSqr/(pi * denom * denom);
+
+  // F
+  float dotLH5 = pow(1.0-dotLH, 5.0);
+  F = F0 + (1.0-F0)*(dotLH5);
+
+  // V
+  float k = alpha/2.0;
+  vis = G1V(dotNL,k)*G1V(dotNV,k);
+
+  float specular = dotNL * D * F * vis;
+  return specular;
+}
+
+float LightingFuncGGX_OPT1(vec3 N, vec3 V, vec3 L, float roughness, float F0)
+{
+  float alpha = roughness*roughness;
+
+  vec3 H = normalize(V+L);
+
+  float dotNL = saturate(dot(N,L));
+  float dotLH = saturate(dot(L,H));
+  float dotNH = saturate(dot(N,H));
+
+  float F, D, vis;
+
+  // D
+  float alphaSqr = alpha*alpha;
+  float pi = 3.14159;
+  float denom = dotNH * dotNH *(alphaSqr-1.0) + 1.0;
+  D = alphaSqr/(pi * denom * denom);
+
+  // F
+  float dotLH5 = pow(1.0-dotLH,5.0);
+  F = F0 + (1.0-F0)*(dotLH5);
+
+  // V
+  float k = alpha/2.0;
+  vis = G1V(dotLH,k)*G1V(dotLH,k);
+
+  float specular = dotNL * D * F * vis;
+  return specular;
+}
+
+float LightingFuncGGX_OPT2(vec3 N, vec3 V, vec3 L, float roughness, float F0)
+{
+  float alpha = roughness*roughness;
+
+  vec3 H = normalize(V+L);
+
+  float dotNL = saturate(dot(N,L));
+
+  float dotLH = saturate(dot(L,H));
+  float dotNH = saturate(dot(N,H));
+
+  float F, D, vis;
+
+  // D
+  float alphaSqr = alpha*alpha;
+  float pi = 3.14159;
+  float denom = dotNH * dotNH *(alphaSqr-1.0) + 1.0;
+  D = alphaSqr/(pi * denom * denom);
+
+  // F
+  float dotLH5 = pow(1.0-dotLH,5.0);
+  F = F0 + (1.0-F0)*(dotLH5);
+
+  // V
+  float k = alpha/2.0;
+  float k2 = k*k;
+  float invK2 = 1.0-k2;
+  vis = inversesqrt(dotLH*dotLH*invK2 + k2);
+
+  float specular = dotNL * D * F * vis;
+  return specular;
+}
+
+vec2 LightingFuncGGX_FV(float dotLH, float roughness)
+{
+  float alpha = roughness*roughness;
+
+  // F
+  float F_a, F_b;
+  float dotLH5 = pow(1.0-dotLH,5.0);
+  F_a = 1.0;
+  F_b = dotLH5;
+
+  // V
+  float vis;
+  float k = alpha/2.0;
+  float k2 = k*k;
+  float invK2 = 1.0-k2;
+  vis = inversesqrt(dotLH*dotLH*invK2 + k2);
+  return vec2(F_a*vis,F_b*vis);
+}
+
+float LightingFuncGGX_D(float dotNH, float roughness)
+{
+  float alpha = roughness*roughness;
+  float alphaSqr = alpha*alpha;
+  float pi = 3.14159;
+  float denom = dotNH * dotNH *(alphaSqr-1.0) + 1.0;
+  float D = alphaSqr/(pi * denom * denom);
+  return D;
+}
+
+float LightingFuncGGX_OPT3(vec3 N, vec3 V, vec3 L, float roughness, float F0)
+{
+  vec3 H = normalize(V+L);
+
+  float dotNL = saturate(dot(N,L));
+  float dotLH = saturate(dot(L,H));
+  float dotNH = saturate(dot(N,H));
+
+  float D = LightingFuncGGX_D(dotNH,roughness);
+  vec2 FV_helper = LightingFuncGGX_FV(dotLH,roughness);
+  float FV = F0*FV_helper.x + (1.0-F0)*FV_helper.y;
+  float specular = dotNL * D * FV;
+  return specular;
+}
+
+float Pow4(float x) {
+  return x*x*x*x;
+}
+
+float LightingFuncGGX_OPT4(vec3 N, vec3 V, vec3 L, float roughness, float F0){
+  vec3 H = normalize(V+L);
+
+  float dotNL = saturate(dot(N,L));
+  float dotLH = saturate(dot(L,H));
+  float dotNH = saturate(dot(N,H));
+
+  float D = texture2D(GgxDFV, vec2(Pow4(dotNH), roughness)).x;
+  vec2 FV_helper = texture2D(GgxDFV, vec2(dotLH,roughness)).yz;
+  float FV = F0*FV_helper.x + (1.0-F0)*FV_helper.y;
+  float specular = dotNL * D * FV;
+
+  return specular;
+}
+
+// This version includes Stephen Hill's optimization
+float LightingFuncGGX_OPT5(vec3 N, vec3 V, vec3 L, float roughness, float F0) {
+  vec3 H = normalize(V+L);
+
+  float dotNL = saturate(dot(N,L));
+  float dotLH = saturate(dot(L,H));
+  float dotNH = saturate(dot(N,H));
+
+  float D = texture2D(GgxDFV, vec2(Pow4(dotNH), roughness)).x;
+  vec2 FV_helper = texture2D(GgxDFV, vec2(dotLH,roughness)).yz;
+
+  float FV = F0*FV_helper.x + FV_helper.y;
+  float specular = dotNL * D * FV;
+
+  return specular;
+}
+
 
 void main(void) {
   vec3 color = vec3(78.0 / 255.0, 58.0 / 255.0, 49.0 / 255.0);
+  color = texture2D(diffuseSampler, vUV).xyz;
   vec3 light_colour = vec3(1.0, 1.0, 1.0);
   vec3 light_direction = normalize(vec3(1.0, 1.0, 1.0));
   
 
   vec3 diffuse = clamp(dot(vNormal, light_direction), 0.0, 1.0) * light_colour;
 
-  float specular_power = 32.0;
-  float specular_colour = 0.02;
-  float normalisation_term = (specular_power + 2.0) / 2.0 * PI;
+  float specular_power = uRoughess;
+  float specular_colour = uSpecularColour;
+  vec3 specular = vec3(0.0, 0.0, 0.0);
+  float specular_term = 0.0;
   vec3 viewDirection = normalize(vEyePosition - vWorldPosition);
-  vec3 halfVector = normalize(viewDirection + light_direction);
-  float n_dot_h = clamp(dot(vNormal, halfVector), 0.0, 1.0);
-  float blinn_phong = pow(n_dot_h, specular_power);    // n_dot_h is the saturated dot product of the normal and half vectors 
-  float specular_term = normalisation_term * blinn_phong;
 
-  float n_dot_l = clamp(dot(vNormal, light_direction), 0.0, 1.0);
-  float cosine_term = n_dot_l;
+  if(type == 0){
+    specular_power = 32.0;
+    specular_colour = 0.03;
 
-  // Dot product of half vector and light vector. No need to saturate as it can't go above 90 degrees
-  float h_dot_l = dot(halfVector, light_direction);
-  float base = 1.0 - h_dot_l;
-  float exponential = pow(base, 5.0);
-  float fresnel_term = specular_colour + (1.0 - specular_colour) * exponential;
-
-  float alpha = 1.0 / (sqrt(PI_OVER_FOUR * specular_power + PI_OVER_TWO));
-  float n_dot_v = clamp(dot(vNormal, vWorldPosition), 0.0, 1.0);
-  float visibility_term = (n_dot_l * (1.0 - alpha) + alpha) * (n_dot_v * (1.0 - alpha) + alpha);
-  visibility_term = 1.0 / visibility_term;
-
-  vec3 specular = (PI / 4.0) * specular_term * cosine_term * fresnel_term * visibility_term * light_colour;
+    float normalisation_term = (specular_power + 2.0) / 2.0 * PI;
+    vec3 halfVector = normalize(viewDirection + light_direction);
+    float n_dot_h = clamp(dot(vNormal, halfVector), 0.0, 1.0);
+    float blinn_phong = pow(n_dot_h, specular_power);    // n_dot_h is the saturated dot product of the normal and half vectors 
+    specular_term = normalisation_term * blinn_phong;
+  
+    float n_dot_l = clamp(dot(vNormal, light_direction), 0.0, 1.0);
+    float cosine_term = n_dot_l;
+  
+    // Dot product of half vector and light vector. No need to saturate as it can't go above 90 degrees
+    float h_dot_l = dot(halfVector, light_direction);
+    float base = 1.0 - h_dot_l;
+    float exponential = pow(base, 5.0);
+    float fresnel_term = specular_colour + (1.0 - specular_colour) * exponential;
+  
+    float alpha = 1.0 / (sqrt(PI_OVER_FOUR * specular_power + PI_OVER_TWO));
+    float n_dot_v = clamp(dot(vNormal, vWorldPosition), 0.0, 1.0);
+    float visibility_term = (n_dot_l * (1.0 - alpha) + alpha) * (n_dot_v * (1.0 - alpha) + alpha);
+    visibility_term = 1.0 / visibility_term;
+  
+    specular = (PI / 4.0) * specular_term * cosine_term * fresnel_term * visibility_term * light_colour;
+  } else if(type == 1) {
+    specular_term = LightingFuncGGX_REF(vNormal, viewDirection, light_direction, uRoughess, uSpecularColour);
+    specular = specular_term * light_colour;
+  } else if(type == 2) {
+    specular_term = LightingFuncGGX_OPT1(vNormal, viewDirection, light_direction, uRoughess, uSpecularColour);
+    specular = specular_term * light_colour;
+  } else if(type == 3) {
+    specular_term = LightingFuncGGX_OPT2(vNormal, viewDirection, light_direction, uRoughess, uSpecularColour);
+    specular = specular_term * light_colour;
+  } else if(type == 4) {
+    specular_term = LightingFuncGGX_OPT3(vNormal, viewDirection, light_direction, uRoughess, uSpecularColour);
+    specular = specular_term * light_colour;
+  } else if(type == 5) {
+    specular_term = LightingFuncGGX_OPT4(vNormal, viewDirection, light_direction, uRoughess, uSpecularColour);
+    specular = specular_term * light_colour;
+  } else if(type == 6) {
+    specular_term = LightingFuncGGX_OPT5(vNormal, viewDirection, light_direction, uRoughess, uSpecularColour);
+    specular = specular_term * light_colour;
+  }
 
 //  options
 //  vec3 slow_hardware_specular = specular_term * cosine_term * light_colour; 
